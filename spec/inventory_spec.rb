ad3b4da84390be5bee6deb010e3b9118d6717e67
Jennifer Bolanos & Jeff Lalic <student@epicodus.com>
2015-05-06 15:06:12 -0700

    houses all inventory levels, saves inventory quantities, and checks equality between different inventory quantities.

--- spec/inventory_spec.rb
+++ spec/inventory_spec.rb
@@ -8,4 +8,26 @@ describe Inventory do
       expect(test_quantity.quantity).to eq(1)
     end
   end
+
+  describe '.all' do
+    it 'is empty at first' do
+      expect(Inventory.all).to eq([])
+    end
+  end
+
+  describe '#save' do
+    it 'will save the quantity value of a book' do
+      test_quantity = Inventory.new(quantity: 3, id: nil)
+      test_quantity.save
+      expect(Inventory.all).to eq([test_quantity])
+    end
+  end
+
+  describe '#==' do
+    it 'is the same quantity value if the quantity and id are the same' do
+      count = Inventory.new(quantity: 3, id: nil)
+      count2 = Inventory.new(quantity:3, id: nil)
+      expect(count).to eq(count2)
+    end
+  end
 end
