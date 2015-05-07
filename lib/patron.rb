32cf96e6c3de51fd683ee65ac38ea78dccb6f739
Jennifer Bolanos & Jeff Lalic <student@epicodus.com>
2015-05-07 10:27:01 -0700

    Will find patron by their ID and return the name.

--- lib/patron.rb
+++ lib/patron.rb
@@ -17,4 +17,18 @@ class Patron
     patrons
   end
 
+  define_method(:save) do
+    result = DB.exec("INSERT INTO patrons (name) VALUES ('#{@name}') RETURNING id;")
+    @id = result.first.fetch("id").to_i
+  end
+
+  define_singleton_method(:find) do |id|
+    result = DB.exec("SELECT * FROM patrons WHERE id = #{id};")
+    name = result.first.fetch("name")
+    Patron.new(name: name, id: id)
+  end
+
+  define_method(:==) do |other_patron|
+    self.name == other_patron.name && self.id == other_patron.id
+  end
 end
--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -15,21 +15,35 @@ describe Patron do
       expect(test_id.id).to eq(1)
     end
   end
+
+  describe '.all' do
+    it 'starts with no patrons' do
+      expect(Patron.all).to eq([])
+    end
+  end
+
+  describe '#save' do
+    it 'saves the patron to the database' do
+      test_patron = Patron.new(name: "Jane", id: 1)
+      test_patron.save
+      expect(Patron.all).to eq([test_patron])
+    end
+  end
+
+  describe '.find' do
+    it 'returns a patron by its ID' do
+      test_patron =  Patron.new(name: "Jane", id: nil)
+      test_patron.save
+      expect(Patron.find(test_patron.id)).to eq(test_patron)
+    end
+
+  end
+
+  describe '#==' do
+    it 'is the same patron if the name and id are the same' do
+      patron1= Patron.new(name: "Malcolm", id: nil)
+      patron2 = Patron.new(name: "Malcolm", id: nil)
+      expect(patron1).to eq(patron2)
+    end
+  end
 end
-#
-#   describe '.all' do
-#     it 'starts with no patrons' do
-#       expect(Patron.all).to eq([])
-#     end
-#   end
-#
-#   describe '.find' do
-#   end
-#
-#
-#   describe '#save' do
-#   end
-#
-#   desribe '#==' do
-#   end
-# end
